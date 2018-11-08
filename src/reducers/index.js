221b6dc7f3aa2b59c591ab65d5f847d3c52e822b
Neil Reyes <rayneilreyes@gmail.com>
2018-11-08 19:21:43 +0800

    Saves reminders to browser cookie

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const reminder = (action) => {
     let { text, dueDate } = action;
@@ -20,10 +21,12 @@ const removeById = (state = [], id) => {
 // Reminders Reducer
 const reminders = ( state = [], action ) => {
     let reminders = null;
+    state = read_cookie('reminders');
 
     switch (action.type){
         case ADD_REMINDER:
             reminders = [...state, reminder(action)];
+            bake_cookie('reminders', reminders);
             console.log('reminder as state: ', reminders);
 
             return reminders;
@@ -31,6 +34,7 @@ const reminders = ( state = [], action ) => {
         
         case DELETE_REMINDER:
             reminders = removeById(state, action.id);
+            bake_cookie('reminders', reminders);
 
             return reminders;
             break;
